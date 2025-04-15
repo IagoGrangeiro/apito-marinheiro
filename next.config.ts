f6e41fdf18e5c1da08e1c6f0e1374c8c1796daff
IagoGrangeiro <90574669+IagoGrangeiro@users.noreply.github.com>
2025-04-15 19:03:30 +0000

    alocacao do appDir para fora de experimental

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  experimental: {
-    //appDir: true, // Habilita o App Router
-  },
+  appDir: true, // Habilita o App Router
 }
  
 export default nextConfig
