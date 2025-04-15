17c951a53ba00144b93ba25ac8ae0ac798f9ccf0
IagoGrangeiro <90574669+IagoGrangeiro@users.noreply.github.com>
2025-04-15 18:53:37 +0000

    Terceira modificacao no next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next'
  
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  experimental: {
+    //appDir: true, // Habilita o App Router
+  },
 }
  
 export default nextConfig
