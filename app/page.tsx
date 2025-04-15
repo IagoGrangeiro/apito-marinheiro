fdd0fb7e002dfe61d0f2e9d4e6638708988631b2
IagoGrangeiro <90574669+IagoGrangeiro@users.noreply.github.com>
2025-04-15 18:03:29 +0000

    Primeira versão

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,94 @@
+"use client";
 import Image from "next/image";
 
 export default function Home() {
+  const scrollToElement = (id: string) => {
+    const element = document.getElementById(id); // Obtém o elemento pelo id
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" }); // Rola suavemente até o elemento
+    }
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
+    <main className="mx-2">
+      <header className="border-b p-4 mb-2">
+        <h1 className="text-xl font-bold text-center">Toques de Apito</h1>
+        <h2 className="text-sm font-bold text-center">Contra-Mestre CEA</h2>
+      </header>
+      <button onClick={() => scrollToElement("apitos-rotina")} className="w-5/6 block cursor-pointer mx-auto rounded-full p-2 border mb-2 bg-sky-300 hover:bg-sky-800 hover:text-white">Apitos de rotina</button>
+      <button onClick={() => scrollToElement("apitos-cerimonia")} className="w-5/6 block cursor-pointer mx-auto rounded-full p-2 border mb-2 bg-sky-300 hover:bg-sky-800 hover:text-white">Apitos de cerimônia</button>
+      
+      {/* Apitos de rotina*/}
+      <div id="apitos-rotina">
+        <h3 className="text-lg font-bold mb-2">Apitos de rotina</h3>
+        <ol className="mx-2 list-decimal list-inside">
+          <li className="mb-2">
+            <p className="inline"><span className="font-bold">Alvorada</span>. Horário: 6:00</p>
+            <audio controls className="mx-auto rounded-full">
+              <source src="alvorada.mp3" type="audio/mpeg"/>
+              Seu navegador não suporta a tag de áudio.
+            </audio> 
           </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+          <li className="mb-2">
+            <p className="inline"><span className="font-bold">Rancho Geral</span>. Horário: 10:40</p>
+            <audio controls className="mx-auto rounded-full"> 
+              <source src="rancho.mp3" type="audio/mpeg"/>
+              Seu navegador não suporta a tag de áudio.
+            </audio> 
+          </li>
+          <li className="mb-2">
+            <p className="inline"><span className="font-bold">Volta</span>. Horário: 14:00</p>
+            <audio controls className="mx-auto rounded-full"> 
+              <source src="volta.mp3" type="audio/mpeg"/>
+              Seu navegador não suporta a tag de áudio.
+            </audio>
+          </li>
+          <li className="mb-2">
+            <p className="inline"><span className="font-bold">Reunir Divisão de Serviço (Parada)</span>. Horário: 17:10</p>
+            <audio controls className="mx-auto rounded-full"> 
+              <source src="parada.mp3" type="audio/mpeg"/>
+              Seu navegador não suporta a tag de áudio.
+            </audio>
+          </li>
+          <li className="mb-2">
+            <p className="inline"><span className="font-bold">Silêncio</span>. Horário: 22:00</p>
+            <audio controls className="mx-auto rounded-full"> 
+              <source src="silencio.mp3" type="audio/mpeg"/>
+              Seu navegador não suporta a tag de áudio.
+            </audio>
           </li>
         </ol>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+      {/* Apitos de cerimônia*/}
+      <div id="apitos-cerimonia">
+        <h3 className="text-lg font-bold mb-2">Apitos de cerimônia (Autoridades)</h3>
+        <ol className="mx-2 list-decimal list-inside">
+          <li className="mb-2">
+            <p className="inline"><span className="font-bold">Of. Superior (Comandante)</span>.</p>
+            <p>Este toque não possui arquivo de áudio disponível no sítio do CIAMA.</p>
+            <p>Segue a partitura com os fonemas.</p>
+            <div>
+              <span className="block underline">RA RI RA  #</span>
+              <span className="block">(LAAAA LAAAA LAAAA LA MI LA # LAAA MI) 2 vezes</span>
+            </div>
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/OfSuperiorCmd.png"
+              alt="Of. Superior"
+              width={800}
+              height={400}
+              className="mx-auto"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          </li>
+          <li className="mb-2">
+            <p className="inline"><span className="font-bold">Of. General</span>.</p>
+            <audio controls className="mx-auto rounded-full">
+              <source src="OfGeneral.mp3" type="audio/mpeg"/>
+              Seu navegador não suporta a tag de áudio.
+            </audio> 
+          </li>
+        </ol>
+      </div>
+    </main>
   );
 }
